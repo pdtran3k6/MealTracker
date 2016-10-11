64058a50e645b996d190e7241098be55bcac117e
Phat Tran <phatdtran3k6@gmail.com>
2016-10-11 10:42:37 -0400

    Added tap twice to take photo feature

--- app/src/main/java/com/example/phatd/mealtracker/MainActivity.java
+++ app/src/main/java/com/example/phatd/mealtracker/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.phatd.mealtracker;
 
 import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.InputConfiguration;
+import android.hardware.camera2.params.OutputConfiguration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.FileProvider;
@@ -12,16 +20,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
 import android.view.View;
-
+import android.hardware.camera2.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public int numTap = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.takePhotos);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Tap again to take photo of a meal", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (numTap == 2)
+                {
+                    Snackbar.make(view, "You tapped twice", Snackbar.LENGTH_LONG).show();
+                    numTap = 1;
+                }
+                else {
+                    Snackbar.make(view, "Tap again to take photo of a meal", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    numTap = 2;
+                }
+
+
             }
+
         });
     }
 
@@ -62,7 +86,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //Request to use camera app for taking photos
+    CameraDevice myCamera;
+
+
+
+    /*//Request to use camera app for taking photos
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
@@ -100,13 +128,15 @@ public class MainActivity extends AppCompatActivity {
         String imageFileName = "Meal_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,   prefix
+                ".jpg",          suffix
+                storageDir       directory
         );
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
-    }
+    }*/
+
+
 }
