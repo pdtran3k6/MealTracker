a8f26ec758be4972e40bff589137538d66df79ff
Phat Tran <phatdtran3k6@gmail.com>
2016-10-13 18:55:58 -0400

    Changed message shown from takePhoto button from snackbar to toast Added contact_me feature

--- app/src/main/java/com/example/phatd/mealtracker/MainActivity.java
+++ app/src/main/java/com/example/phatd/mealtracker/MainActivity.java
@@ -1,6 +1,23 @@
 package com.example.phatd.mealtracker;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CaptureRequest;
@@ -11,24 +28,36 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.test.mock.MockContext;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Surface;
 import android.view.View;
 import android.hardware.camera2.*;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import static android.app.PendingIntent.getActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,23 +73,20 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.takePhotos);
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                if (numTap == 2)
-                {
-                    Snackbar.make(view, "You tapped twice", Snackbar.LENGTH_LONG).show();
+                if (numTap == 2) {
+                    Toast.makeText(MainActivity.this, "You tapped twice",
+                            Toast.LENGTH_SHORT).show();
+                    //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     numTap = 1;
                 }
                 else {
-                    Snackbar.make(view, "Tap again to take photo of a meal", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    Toast.makeText(MainActivity.this, "Tap again to take photo of a meal",
+                            Toast.LENGTH_SHORT).show();
                     numTap = 2;
                 }
-
-
             }
-
         });
     }
 
@@ -79,26 +105,48 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.contact_me) {
+            String[] addresses = {"phatdtran3k6@gmail.com"};
+            Intent takePictureIntent = new Intent(Intent.ACTION_SENDTO);
+            takePictureIntent.setData(Uri.parse("mailto:"));
+            takePictureIntent.putExtra(Intent.EXTRA_EMAIL, addresses);
+            takePictureIntent.putExtra(Intent.EXTRA_SUBJECT, "SUGGESTION");
+            takePictureIntent.putExtra(Intent.EXTRA_TEXT, "Hi Phat, you're cute.");
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(takePictureIntent);
+            }
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    CameraDevice myCamera;
-
-
-
-    /*//Request to use camera app for taking photos
+    //Request to use camera app for taking photos
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    //Save full-sized photos on the phone
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException, IOException {
+        // Create an image file name that is collision-resistant
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Meal_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,   //prefix
+                ".jpg",          //suffix
+                storageDir       //directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         //Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-
             // Create the File where the photo should go
             File photoFile = null;
             try {
@@ -111,32 +159,11 @@ public class MainActivity extends AppCompatActivity {
             // Continue only if the File was successfully created
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
+                        "com.example.phatd.mealtracker.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, RESULT_OK);
             }
         }
     }
-
-    //Save full-sized photos in the phone
-    String mCurrentPhotoPath;
-
-    private File createImageFile() throws IOException, IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "Meal_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,   prefix
-                ".jpg",          suffix
-                storageDir       directory
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }*/
-
-
 }
